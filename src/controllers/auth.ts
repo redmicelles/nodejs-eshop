6a440701f1328f3f0d4652c981f0e1045565b186
Seyi Daniel <dexter_daniel@outlook.com>
2024-08-16 20:34:16 +0100

    Implemented login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,13 @@
 import {Request, Response} from 'express';
 import { prismaClient } from '..';
-import {hashSync} from 'bcrypt';
+import {hashSync, compareSync} from 'bcrypt';
+import * as jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../secrets';
 
 export const signup = async (req: Request, res: Response) => {
     const {name, email, password} = req.body;
 
-    let user = await prismaClient.user.findFirst({where: {email}})
+    let user = await prismaClient.user.findFirst({where: {email}});
     if (user) {
         throw Error('User already exists')
     }
@@ -15,6 +17,26 @@ export const signup = async (req: Request, res: Response) => {
             email,
             password: hashSync(password, 10)
         }
-    })
-    res.json(user)
+    });
+    res.json(user);
+}
+
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+
+    let user = await prismaClient.user.findFirst({where: {email}});
+
+    if (!user || !compareSync(password, user.password)) {
+        throw Error('Invalid username or password!');
+    }
+    
+    const token = jwt.sign({
+        sub: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.createdAt
+    }, JWT_SECRET)
+
+    res.json({token});
 }
